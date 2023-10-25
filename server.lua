abb0994ca882e5367d34d29050b101a0a365007c
byK3 <92488474+byK3@users.noreply.github.com>
2023-10-25 03:17:25 +0200

    Added daily reward code

--- server.lua
+++ server.lua
@@ -4,15 +4,39 @@ RegisterCommand(Config.command, function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     local steamName = GetPlayerName(source)
 
-    if not claimedRewards[xPlayer.identifier] then
-        if string.match(steamName, Config.serverName) then
-            giveReward(xPlayer.source)
-            claimedRewards[xPlayer.identifier] = true
-        else
-            Notify (xPlayer.source, string.format(Config.lang["wrong_name"]))
-        end
+    if Config.dailyReward then
+        local xIdentifier = xPlayer.identifier
+        MySQL.Async.fetchAll('SELECT last_claimed FROM k3_steamRewards WHERE identifier = @identifier', {
+            ['@identifier'] = xIdentifier
+        }, function(result)
+            local lastClaimed = result[1] and result[1].last_claimed or nil
+
+            if lastClaimed and os.date('%d.%m.%Y') <= lastClaimed then
+                Notify(xPlayer.source, Config.lang["already_claimed"])
+            else
+                if string.match(steamName, Config.serverName) then
+                    giveReward(xPlayer.source)
+                    MySQL.Async.execute('REPLACE INTO k3_steamRewards (identifier, last_claimed) VALUES (@identifier, @today)', {
+                        ['@identifier'] = xIdentifier,
+                        ['@today'] = os.date('%d.%m.%Y')
+                    })
+                    Notify(xPlayer.source, Config.lang["claimed"])
+                else
+                    Notify(xPlayer.source, Config.lang["wrong_name"])
+                end
+            end
+        end)
     else
-        Notify (xPlayer.source, string.format(Config.lang["already_claimed"]))
+        if not claimedRewards[xPlayer.identifier] then
+            if string.match(steamName, Config.serverName) then
+                giveReward(xPlayer.source)
+                claimedRewards[xPlayer.identifier] = true
+            else
+                Notify(xPlayer.source, Config.lang["wrong_name"])
+            end
+        else
+            Notify(xPlayer.source, Config.lang["already_claimed"])
+        end
     end
 end)
 
